78c436b721f4fb5b119e66dbfa020132881a83e7
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 11:29:16 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+export { default as Text } from './text';
+export { default as Button } from './button';
+export { default as Container } from './container';
+export { default as Radio } from './radio';
+export { default as Checkbox } from './checkbox';
